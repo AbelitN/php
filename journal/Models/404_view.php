4b9fa94879a2dde2ddad906263eee4be41877e27
AbelitN <105322125+AbelitN@users.noreply.github.com>
2022-05-11 00:50:54 +0300

    Add files via upload

---
+++ journal/Models/404_view.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html> 
+<html lang="ru"> 
+<head> 
+ <meta charset="utf-8"> 
+ <title>Главная</title> 
+</head> 
+<body> 
+ <?php include 'views/'.$content_view; ?> 
+ <h1>404!</h1> 
+</body> 
+</html> 
